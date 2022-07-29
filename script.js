c00bbadccf48eb21a2a443285a8eb79336317ccf
Thiago Dias <thiago@thiago.com>
2022-07-29 19:11:37 -0300

    iniciado a logica progressBar

--- script.js
+++ script.js
@@ -5,14 +5,11 @@ const stop = document.querySelector('.icon__stop');
 const progressBar = document.querySelector('.progress__bar');
 
 let minhaHora = new Date();
+minhaHora.setHours(00,00,00,00);
+let progressBarWidth = 0;
 
 let bitTrava = false;
 
-
-
-minhaHora.setHours(00,00,00,00);
-
-progressBar.setAttribute("style","width:500px");
 
 
 const addMinute = () => {
@@ -73,6 +70,7 @@ if(!bitTrava){
         setIntervalId = setInterval(() => {
             reduceSecond();
             showTime();
+            incrementaProgressBar1();
         }, 100);
 
         bitTrava =true;
@@ -81,9 +79,8 @@ if(!bitTrava){
     }else{
         alert("Insira um tempo antes de iniciar o temporizador!")
     }
-
 }
-      
+
 })
 
 
@@ -101,3 +98,28 @@ pause.addEventListener('click', ()=>{
     bitTrava = false;
 })
 
+
+
+function incrementaProgressBar1() {
+   
+   if(progressBarWidth < 100) {
+    progressBarWidth ++;
+    console.log(progressBarWidth);
+   }
+   progressBar.setAttribute("style","width:"+ progressBarWidth +"%");
+}
+
+function incrementaProgressBar10() {
+    if(progressBarWidth < 100) progressBarWidth += 10 ;
+    progressBar.setAttribute("style","width:"+ progressBarWidth +"%");
+ }
+
+ function incrementaProgressBar25() {
+    if(progressBarWidth < 100) progressBarWidth += 25 ;
+    progressBar.setAttribute("style","width:"+ progressBarWidth +"%");
+ }
+
+ function incrementaProgressBar50() {
+    if(progressBarWidth < 100) progressBarWidth += 50 ;
+    progressBar.setAttribute("style","width:"+ progressBarWidth +"%");
+ }
